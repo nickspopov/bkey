b7bd984c0fb7fa3783bda9b61827e75dde6069de
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2026-02-06 19:15:01 +0100

    Initial Commit

---
+++ bkey/bkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bkeyApp.swift
+//  bkey
+//
+//  Created by Nick Popov on 06.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct bkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bkeyTests/bkeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  bkeyTests.swift
+//  bkeyTests
+//
+//  Created by Nick Popov on 06.02.2026.
+//
+
+import Testing
+@testable import bkey
+
+struct bkeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
